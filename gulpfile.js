8f519640877163f3aa411f2f4dfb7600b502f8a6
Allen Zheng <strburst@users.noreply.github.com>
2017-01-24 09:22:09 -0500

    Set up eslint through gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,36 @@
-var gulp = require('gulp')
-var concat = require('gulp-concat')
+/* eslint arrow-body-style: "off" */
 
-gulp.task('build:js', function () {
+const gulp = require('gulp');
+// This will put all "gulp-*" modules into $
+const $ = require('gulp-load-plugins')({
+  DEBUG: process.env.DEBUG,
+});
+
+gulp.task('lint:js', () => {
+  gulp.src(['app/gulp/js/**/*.js', '!node_modules/**', '!vendor/**'])
+    .pipe($.eslint())
+    .pipe($.eslint.format())
+    .pipe($.eslint.failAfterError());
+});
+
+gulp.task('build:js', ['lint:js'], () => {
   return gulp.src('app/gulp/js/**/*.js')
-    .pipe(concat('app.js'))
-    .pipe(gulp.dest('vendor/assets/javascripts/gulp/'))
-})
+    .pipe($.concat('app.js'))
+    .pipe(gulp.dest('vendor/assets/javascripts/gulp/'));
+});
 
-gulp.task('build:css', function () {
+gulp.task('build:css', () => {
   return gulp.src('app/gulp/css/**/*.css')
-    .pipe(gulp.dest('vendor/assets/stylesheets/gulp/'))
-})
+    .pipe(gulp.dest('vendor/assets/stylesheets/gulp/'));
+});
 
-gulp.task('watch:js', function () {
-  gulp.watch('app/gulp/js/**/*.js', ['build:js'])
-})
+gulp.task('watch:js', () => {
+  gulp.watch('app/gulp/js/**/*.js', ['build:js']);
+});
 
-gulp.task('watch:css', function () {
-  gulp.watch('app/gulp/css/**/*.css', ['build:css'])
-})
+gulp.task('watch:css', () => {
+  gulp.watch('app/gulp/css/**/*.css', ['build:css']);
+});
 
-gulp.task('watch', ['watch:js', 'watch:css'])
-gulp.task('build', ['build:js', 'build:css'])
+gulp.task('watch', ['watch:js', 'watch:css']);
+gulp.task('build', ['build:js', 'build:css']);
