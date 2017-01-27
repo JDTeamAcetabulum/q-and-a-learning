df323bcd74e57cd3257e7c45b2792ba0d6eb7b1d
rmkeezer <rmkeezer@yahoo.com>
2017-01-27 02:02:19 -0500

    Fixed bug in login redirect

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -5,7 +5,7 @@ class LoginController < ApplicationController
   def create
     user = User.find_by(username: params[:login][:username])
     if user && user.authenticate(params[:login][:password])
-      log_in user
+      view_context.log_in user
       redirect_to user
     else
       flash.now[:danger] = 'Invalid email/password combination'
--- app/helpers/login_helper.rb
+++ app/helpers/login_helper.rb
@@ -2,12 +2,12 @@ module LoginHelper
 
   # Logs in the given user.
   def log_in(user)
-    login[:user_id] = user.id
+    session[:user_id] = user.id
   end
 
   # Returns the current logged-in user (if any).
   def current_user
-    @current_user ||= User.find_by(id: login[:user_id])
+    @current_user ||= User.find_by(id: session[:user_id])
   end
 
   # Returns true if the user is logged in, false otherwise.
@@ -17,7 +17,7 @@ module LoginHelper
 
   # Logs out the current user.
   def log_out
-    login.delete(:user_id)
+    session.delete(:user_id)
     @current_user = nil
   end
 end
