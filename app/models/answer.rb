01ee0f03bf90e65c7477744c63731136757126c5
rmkeezer <rmkeezer@yahoo.com>
2017-02-08 13:38:41 -0500

    removed not working Answer attributes

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -2,20 +2,4 @@ class Answer < ApplicationRecord
 
   belongs_to :question
 
-  validates :content, presence: true
-
-  ATTRIBUTES = %I(correct content)
-  attr_accessible(*ATTRIBUTES)
-
-  DEFAULTS = {
-    correct: false,
-    content: ""
-  }
-
-  def initialize(opts = {})
-    DEFAULTS.
-      merge(opts).
-      slice(*ATTRIBUTES).
-      each { |k,v| instance_variable_set("@#{k}", v) }
-  end
 end
