854e812ccbd5017c06e0c1077b2d79929b87a45e
rmkeezer <rmkeezer@yahoo.com>
2017-04-17 02:06:29 -0400

    Fixed js listeners and fixed creating a new question javascript

--- app/gulp/js/questions.js
+++ app/gulp/js/questions.js
@@ -1,23 +1,24 @@
 // Code to control adding/removing answer choices
-onPage('questions', 'edit', () => {
-  let num = 10000; // We only support 10000 answers per question, get over it
-  $('#add-answer').click((e) => {
-    num -= 1;
-    e.preventDefault();
-    // Regexes here modify the duplicated div to create a unique hash
-    // (by decrementing 1 from 10000)
-    let newAnswer = $('#wrong-answers div:first-child').prop('outerHTML');
-    newAnswer = newAnswer.replace(/_[0-9]+_/g, `_${num}_`);
-    newAnswer = newAnswer.replace(/\[[0-9]+\]/g, `[${num}]`);
-    newAnswer = newAnswer.replace(/value="*."/g, 'value=""');
-    $('#wrong-answers').append(newAnswer);
-  });
-
-  $('.remove-answer').click((e) => {
-    e.preventDefault();
-    $(e.target).parent().remove();
-  });
+// onPage('questions', 'edit', () => {
+// Needs to be on both edit AND new
+let num = 10000; // We only support 10000 answers per question, get over it
+$(document).on('click', '#add-answer', (e) => {
+  num -= 1;
+  e.preventDefault();
+  // Regexes here modify the duplicated div to create a unique hash
+  // (by decrementing 1 from 10000)
+  let newAnswer = $('#wrong-answers div:last-child').prop('outerHTML');
+  newAnswer = newAnswer.replace(/_[0-9]+_/g, `_${num}_`);
+  newAnswer = newAnswer.replace(/\[[0-9]+\]/g, `[${num}]`);
+  newAnswer = newAnswer.replace(/value="*."/g, 'value=""');
+  $('#wrong-answers').append(newAnswer);
 });
+
+$(document).on('click', 'button.remove-answer', (e) => {
+  e.preventDefault();
+  $(e.target).parent().remove();
+});
+// });
 
 $('body.questions.export').ready(() => {
   $('#by-lecture').click((e) => {
@@ -49,37 +50,37 @@ onPage('questions', 'show', () => {
     const height = 250 - margin.top - margin.bottom;
 
     const y = d3.scaleBand()
-      .range([height, 0])
-      .padding(0.1);
+    .range([height, 0])
+    .padding(0.1);
 
     const x = d3.scaleLinear()
-      .range([0, width]);
+    .range([0, width]);
 
     const svg = d3.select('#graph')
-        .attr('width', width + margin.left + margin.right)
-        .attr('height', height + margin.top + margin.bottom)
-      .append('g')
-        .attr('transform',
-              `translate(${margin.left},${margin.top})`);
+      .attr('width', width + margin.left + margin.right)
+      .attr('height', height + margin.top + margin.bottom)
+    .append('g')
+      .attr('transform',
+            `translate(${margin.left},${margin.top})`);
 
     x.domain([0, d3.max(data, d => d.value)]);
     y.domain(data.map(d => d.label));
 
     svg.selectAll('.bar')
-        .data(data)
-      .enter().append('rect')
-        .attr('class', 'bar')
-        .attr('width', d => x(d.value))
-        .attr('y', d => y(d.label))
-        .attr('height', y.bandwidth())
-        .style('fill', d => color(d));
+      .data(data)
+    .enter().append('rect')
+      .attr('class', 'bar')
+      .attr('width', d => x(d.value))
+      .attr('y', d => y(d.label))
+      .attr('height', y.bandwidth())
+      .style('fill', d => color(d));
 
     svg.append('g')
-        .attr('transform', `translate(0,${height})`)
-        .call(d3.axisBottom(x));
+      .attr('transform', `translate(0,${height})`)
+      .call(d3.axisBottom(x));
 
     svg.append('g')
-        .call(d3.axisLeft(y));
+      .call(d3.axisLeft(y));
   }
 
   function answerQuestion() {
