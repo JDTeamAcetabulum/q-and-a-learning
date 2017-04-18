120a77204feaade63037e32127800006289b080d
Allen Zheng <strburst@users.noreply.github.com>
2017-04-18 19:40:56 -0400

    Flesh out description text on the homepage (#148)
    This patch gets rid of the placeholder text on the homepage and replaces
    it with some help text.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,4 +9,10 @@ module ApplicationHelper
       page_title + " | " + base_title
     end
   end
+
+  # Create an li tag with a bolded section, an emdash, then more text.
+  def bold_list_item(bolded, description)
+    "<li><strong>#{bolded}</strong> &mdash; #{description}</li>".html_safe
+  end
+
 end
